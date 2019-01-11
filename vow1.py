6627a752d5bcad3a46120978449f27b06c80ea00
snehagovind <46587543+snehagovind@users.noreply.github.com>
2019-01-11 11:05:24 +0530

    Create vow1.py

---
+++ vow1.py
@@ -0,0 +1,10 @@
+ch=input()
+
+if(ch=='A' or ch=='a' or ch=='E' or ch =='e' or ch=='I'
+ or ch=='i' or ch=='O' or ch=='o' or ch=='U' or ch=='u'):
+    print( "Vowel")
+elif(ch=="$"):
+    print("invalid")
+else:
+    print("Consonant")
+    
